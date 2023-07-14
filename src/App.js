975e9bbe87def73b406954ff169416e75af5cb34
PREETAM <preetamvarun99@gmail.com>
2023-07-14 14:46:52 +0100

    rotated the right bottom div to 90 degrees

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import vectorA from './Assets/vector-A.svg';
+import vectorA1 from './Assets/VECTOR-A1.svg';
+import vectorL from './Assets/vector-L.svg';
+import vectorM from './Assets/vector-M.svg';
+import vectorO from './Assets/vector-O.svg';
+import vectorP from './Assets/vector-P.svg';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+    <div className='App overflow-hidden'>
+      <div className='inline-block relative top-1/2 translate-y-[-50%] translate-x-10'>
+        <img src={vectorA} alt='a' className='h-[85px] w-[90px]' />
+        <img src={vectorM} alt='a' className='h-[85px] w-[90px]' />
+        <img src={vectorO} alt='a' className='h-[85px] w-[90px]' />
+        <img src={vectorL} alt='a' className='h-[85px] w-[90px]' />
+        <img src={vectorA1} alt='a' className='h-[85px] w-[90px]' />
+        <img src={vectorP} alt='a' className='h-[85px] w-[90px]' />
+      </div>
+      <div className='text-white absolute right-0 bottom-0 origin-bottom-left -rotate-90 translate-x-full text-[rgba(255,255,255,0.75)]'>
+        <p className='text-[70px] tracking-[6.6px]'>HEAR</p>
+        <p className='text-[70px] tracking-[6.6px]'>LATEST</p>
+        <p className='text-[70px] tracking-[6.6px]'>TOUR</p>
+        <p className='text-[70px] tracking-[6.6px]'>SEE</p>
+        <p className='text-[70px] tracking-[6.6px]'>SHOP</p>
+        <p className='text-[70px] tracking-[6.6px]'>WATCH</p>
+        <p className='text-[70px] tracking-[6.6px]'>PERSONAL</p>
+        <p className='text-[70px] tracking-[6.6px]'>AID</p>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Vector.js
@@ -0,0 +1,5 @@
+const Vector = ({ vectorImg }) => {
+  return <img src={vectorImg} alt='img' className='h-[85px] w-[90px]' />;
+};
+
+export default Vector;
